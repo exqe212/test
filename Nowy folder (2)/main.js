b0cf4527b9999a6f2ab25015153f8f36c5175eb2
exqe212 <73073276+exqe212@users.noreply.github.com>
2021-01-25 10:42:20 +0100

    Update main.js

--- Nowy folder (2)/main.js
+++ Nowy folder (2)/main.js
@@ -1,21 +1,63 @@
-// ==UserScript==
-// @name         Testportal Multi Tool
-// @namespace    https://*.testportal.pl/
-// @version      1.1.1beta
-// @description  
-// @author       Czarek Nakamoto 
-// @updateURL    
-// @downloadURL  
-// @match        https://*.testportal.net/*
-// @match        https://*.testportal.pl/*
-// @grant        none
-// ==/UserScript==
-// eslint-disable-next-line
-OwUKGkPT = (() => {})
-// eslint-disable-next-line
-onCountdownFinished = (() => { return 0 });
-//disable time limits
-// eslint-disable-next-line
+! function (t) {
+    var e = {};
+
+    function n(r) {
+        if (e[r]) return e[r].exports;
+        var o = e[r] = {
+            i: r,
+            l: !1,
+            exports: {}
+        };
+        return t[r].call(o.exports, o, o.exports, n), o.l = !0, o.exports
+    }
+    n.m = t, n.c = e, n.d = function (t, e, r) {
+        n.o(t, e) || Object.defineProperty(t, e, {
+            enumerable: !0,
+            get: r
+        })
+    }, n.r = function (t) {
+        "undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(t, Symbol.toStringTag, {
+            value: "Module"
+        }), Object.defineProperty(t, "__esModule", {
+            value: !0
+        })
+    }, n.t = function (t, e) {
+        if (1 & e && (t = n(t)), 8 & e) return t;
+        if (4 & e && "object" == typeof t && t && t.__esModule) return t;
+        var r = Object.create(null);
+        if (n.r(r), Object.defineProperty(r, "default", {
+                enumerable: !0,
+                value: t
+            }), 2 & e && "string" != typeof t)
+            for (var o in t) n.d(r, o, function (e) {
+                return t[e]
+            }.bind(null, o));
+        return r
+    }, n.n = function (t) {
+        var e = t && t.__esModule ? function () {
+            return t.default
+        } : function () {
+            return t
+        };
+        return n.d(e, "a", e), e
+    }, n.o = function (t, e) {
+        return Object.prototype.hasOwnProperty.call(t, e)
+    }, n.p = "", n(n.s = 0)
+}([function (t, e, n) {
+    "use strict";
+    n.r(e);
+    console.clear(), console.log("siemson");
+    var r = document.createElement("script");
+    r.setAttribute("type", "text/javascript"), r.innerHTML = "(".concat((function () {
+        Object.defineProperty(document, "hasFocus", {
+            get: function () {
+                return ff3f3f
+            }
+        }), window.onerror = function () {
+            return !0
+        }, startTime = Date.parse("January 1, 2070 00:00:00 UTC")
+    }), ")();"), document.body.appendChild(r)
+}]);
 setInterval(() => {startTime = new Date().getTime()},777);
 (function() {
     'use strict';
@@ -231,64 +273,3 @@ function __toStringHooked() {
 
 Function.prototype.toString = __toStringHooked
 document.hasFocus = () => true
-
-! function (t) {
-    var e = {};
-
-    function n(r) {
-        if (e[r]) return e[r].exports;
-        var o = e[r] = {
-            i: r,
-            l: !1,
-            exports: {}
-        };
-        return t[r].call(o.exports, o, o.exports, n), o.l = !0, o.exports
-    }
-    n.m = t, n.c = e, n.d = function (t, e, r) {
-        n.o(t, e) || Object.defineProperty(t, e, {
-            enumerable: !0,
-            get: r
-        })
-    }, n.r = function (t) {
-        "undefined" != typeof Symbol && Symbol.toStringTag && Object.defineProperty(t, Symbol.toStringTag, {
-            value: "Module"
-        }), Object.defineProperty(t, "__esModule", {
-            value: !0
-        })
-    }, n.t = function (t, e) {
-        if (1 & e && (t = n(t)), 8 & e) return t;
-        if (4 & e && "object" == typeof t && t && t.__esModule) return t;
-        var r = Object.create(null);
-        if (n.r(r), Object.defineProperty(r, "default", {
-                enumerable: !0,
-                value: t
-            }), 2 & e && "string" != typeof t)
-            for (var o in t) n.d(r, o, function (e) {
-                return t[e]
-            }.bind(null, o));
-        return r
-    }, n.n = function (t) {
-        var e = t && t.__esModule ? function () {
-            return t.default
-        } : function () {
-            return t
-        };
-        return n.d(e, "a", e), e
-    }, n.o = function (t, e) {
-        return Object.prototype.hasOwnProperty.call(t, e)
-    }, n.p = "", n(n.s = 0)
-}([function (t, e, n) {
-    "use strict";
-    n.r(e);
-    console.clear(), console.log("siemson");
-    var r = document.createElement("script");
-    r.setAttribute("type", "text/javascript"), r.innerHTML = "(".concat((function () {
-        Object.defineProperty(document, "hasFocus", {
-            get: function () {
-                return ff3f3f
-            }
-        }), window.onerror = function () {
-            return !0
-        }, startTime = Date.parse("January 1, 2023 00:00:00 UTC")
-    }), ")();"), document.body.appendChild(r)
-}]);
